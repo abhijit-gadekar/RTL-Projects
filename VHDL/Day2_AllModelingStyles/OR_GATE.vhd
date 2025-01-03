7526c523b428d0e858ffd076caafe5988ef91859
Abhijit Gadekar <54983497+abhijit-gadekar@users.noreply.github.com>
2025-01-04 00:32:34 +0100

    RTL-Projects

---
+++ VHDL/Day2_AllModelingStyles/OR_GATE.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity OR_GATE is
+    Port ( A, B : in STD_LOGIC;
+           Y : out STD_LOGIC);
+end OR_GATE;
+
+architecture Behavioral of OR_GATE is
+begin
+    Y <= A or B;
+end Behavioral;
