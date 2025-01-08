080072564f258c8afc51edafca58ca6957beacd7
Abhijit Gadekar <54983497+abhijit-gadekar@users.noreply.github.com>
2025-01-08 22:26:31 +0100

    RTL-Projects

--- VHDL/Day6_FullAdder/HalfAdder.vhd
+++ VHDL/Day6_FullAdder/HalfAdder.vhd
@@ -2,10 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity HalfAdder is
+    
     Port ( A     : in  STD_LOGIC;
            B     : in  STD_LOGIC;
            Sum   : out STD_LOGIC;
            Cout  : out STD_LOGIC);
+    
 end HalfAdder;
 
 architecture Behavioral of HalfAdder is
