475f9518591a57a1c1ae5bddc92ff134a001ec2a
Abhijit Gadekar <54983497+abhijit-gadekar@users.noreply.github.com>
2025-01-22 23:58:49 +0100

    RTL-Projects

---
+++ VHDL/Day18_2x1Mux/mux2to1.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mux2to1 is
+    Port (
+        a : in STD_LOGIC;      
+        b : in STD_LOGIC;         
+        sel : in STD_LOGIC;       
+        y : out STD_LOGIC         
+    );
+end mux2to1;
+
+architecture Behavioral of mux2to1 is
+begin
+    process(a, b, sel)
+    begin
+        if sel = '0' then
+            y <= a;              
+        else
+            y <= b;             
+        end if;
+    end process;
+end Behavioral;
